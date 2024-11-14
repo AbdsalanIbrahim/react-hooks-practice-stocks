9a94e3a233e11cf0c992c25cce6f77e00b80ea85
Abdisalan Ibrahim <crgmelo22@gmail.com>
2024-11-14 15:49:30 +0300

    .

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,22 +1,23 @@
-import React from "react";
-import StockContainer from "./StockContainer";
-import PortfolioContainer from "./PortfolioContainer";
-import SearchBar from "./SearchBar";
+const [sortOption, setSortOption] = useState("Alphabetically");
+const [filterOption, setFilterOption] = useState("All");
 
-function MainContainer() {
-  return (
-    <div>
-      <SearchBar />
-      <div className="row">
-        <div className="col-8">
-          <StockContainer />
-        </div>
-        <div className="col-4">
-          <PortfolioContainer />
-        </div>
-      </div>
-    </div>
-  );
-}
+const handleSortChange = (option) => setSortOption(option);
+const handleFilterChange = (type) => setFilterOption(type);
 
-export default MainContainer;
+const filteredStocks = stocks
+  .filter((stock) => filterOption === "All" || stock.type === filterOption)
+  .sort((a, b) => {
+    if (sortOption === "Alphabetically") {
+      return a.ticker.localeCompare(b.ticker);
+    } else {
+      return a.price - b.price;
+    }
+  });
+
+return (
+  <div>
+    <SearchBar onSortChange={handleSortChange} onFilterChange={handleFilterChange} />
+    <StockContainer stocks={filteredStocks} onBuyStock={handleBuyStock} />
+    <PortfolioContainer portfolio={portfolio} onSellStock={handleSellStock} />
+  </div>
+);
